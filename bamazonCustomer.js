9df0f6acc975ff229d89a8f64354fcfc06576c5d
Paola L <paolalarrazabal@gmail.com>
2019-12-13 15:48:49 -0500

    created database and products table, added connection variable with database info, added connection function and function queryAllProducts to display table info

---
+++ bamazonCustomer.js
@@ -0,0 +1,34 @@
+const mysql = require ("mysql"); 
+const inquirer = require ("inquirer"); 
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306, 
+    user: "root",
+    password: "TT11lala:*", 
+    database: "bamazon"
+});
+
+
+connection.connect(function(err){
+    if (err) throw err; 
+    console.log(`connected as id ${connection.threadId}`); 
+    queryAllProducts()
+    
+    
+}); 
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err; 
+            
+        for (let i = 0; i < res.length; i++) {
+            console.log(`${res[i].item_id} | ${res[i].product_name} | ${res[i].department_name} | ${res[i].price} | ${res[i].stock_quantity}` );
+        }
+        console.log("---------------------------")
+
+        connection.end();
+            
+    })
+}
