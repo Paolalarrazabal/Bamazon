39893ba71f2e4ec8f63c074b51b83e1f12db0a9a
Paola L <paolalarrazabal@gmail.com>
2019-12-14 04:44:27 -0500

    fixed sql syntax error by adding  quotes around item_id, moved query variable to inside of .then inquirer function added if statement for prompt questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,24 +11,95 @@ const connection = mysql.createConnection({
 });
 
 
-connection.connect(function(err){
-    if (err) throw err; 
-    console.log(`connected as id ${connection.threadId}`); 
-    queryAllProducts()
+
+
+queryAllProducts();
     
     
-}); 
+
 
 function queryAllProducts() {
-    connection.query("SELECT * FROM products", function (err, res) {
+
+    connection.connect(function(err){
+        if (err) throw err; 
+        console.log(`connected as id ${connection.threadId}`); 
+    
+    connection.query("SELECT * FROM `products`", function (err, res) {
         if (err) throw err; 
             
         for (let i = 0; i < res.length; i++) {
             console.log(`${res[i].item_id} | ${res[i].product_name} | ${res[i].department_name} | ${res[i].price} | ${res[i].stock_quantity}` );
         }
         console.log("---------------------------")
-
-        connection.end();
+    
+        productSelection(); 
+        
             
-    })
+    });
+});
+    
 }
+
+function productSelection() {
+    inquirer
+    .prompt([ 
+        {
+            type: "input",
+            message: "Hi, what is the product ID?",
+            name: "productID",   
+            filter: Number
+        }, 
+        {
+            type: "input",
+            message: "How many of these bad boys would you like?",
+            name: "productQuantity", 
+            filter: Number
+        }
+    ])
+    .then(function (user) {
+
+        let purchaseAmount = user.productQuantity; 
+        console.log("you entered " + user.productID); 
+
+        const query = connection.query("SELECT * FROM `products` WHERE `item_id` =" + user.productID, function (err, res) {
+        if (err) throw err; 
+        
+        for (let i = 0; i < res.length; i++) {
+            console.log(`This is your product: ${res[i].product_name}`);
+        
+
+            if (purchaseAmount <= res[0].stock_quantity) {
+
+                totalPrice = purchaseAmount * res[i].price; 
+
+                console.log(`Your total is: ${totalPrice}, Thank you for shopping with us`);
+            } else {
+                console.log(`Sorry, we do not have enough ${res[i].product_name} in stock to fulfill this order` );
+            }
+           
+         }
+
+         
+
+        
+
+            
+    }); 
+
+        
+    
+    console.log(query.sql);
+    connection.end();  
+     
+ 
+
+    });
+
+}
+
+
+
+
+
+
+
